bf5a69957d4b655cd8006043ecd18e29b23506b0
Prodlax <50209414+Prodlax@users.noreply.github.com>
2019-06-04 09:18:22 -0700

    Update project.c

--- project.c
+++ project.c
@@ -1,52 +1,50 @@
 #include <stdio.h>
 
-int main()
+int
+main ()
 {
-    /* C program to initialize arrayGain and arrayLoss */
-    int arrayGain[30];
-    for (int i = 0; i < 30; i++)
-        {
-                 printf("Input money earned on day %d: \n", i);
-                 scanf("%d", &arrayGain[i]);
-        }
-    int arraySpent[30];
-    for (int i = 0; i < 30; i++)
-        {
-                 printf("Input money spent on day %d: \n", i);
-                 scanf("%d", &arraySpent[i]);
-        }
-}  
+  /* C program to initialize arrayGain and arrayLoss */
+  int arrayGain[30];
+  for (int i = 0; i < 30; i++)
+    {
+      printf ("Input money earned on day %d: \n", i);
+      scanf ("%d", &arrayGain[i]);
+    }
+  int arraySpent[30];
+  for (int i = 0; i < 30; i++)
+    {
+      printf ("Input money spent on day %d: \n", i);
+      scanf ("%d", &arraySpent[i]);
+    }
 
 /**
  * C program to calculate gain or loss
  */
-int main()
-{
-    int ms, me, amt; 
-    
-    /* Input money spent and money earned */
-    printf("Enter money spent today: ");
-    scanf("%d", &ms);
-    printf("Enter money earned today: ");
-    scanf("%d", &me);
-    
-    if(me > ms)
+  int ms, me, amt;
+
+  /* money spent and money earned, daily */
+  for (int h = 0; h < 30; h++)
     {
-        /* Calculate Gain */
-        amt = me - ms;
-        printf("Profit = %d", amt);
+      ms = arraySpent[h];
+      me = arrayGain[h];
+
+      if (me > ms)
+	{
+	  /* Calculate Gain */
+	  amt = me - ms;
+	  printf ("Profit day %d = %d\n", h, amt);
+	}
+
+      else if (ms > me)
+	{
+	  /* Calculate Loss */
+	  amt = ms - me;
+	  printf ("Loss day %d = %d\n", h, amt);
+	}
+      else
+	{
+	  /* Neither gain nor loss */
+	  printf ("No profit or loss day %d", h);
+	}
     }
-    
-    return 0;
 }
-    else if(ms > me)
-    {
-        /* Calculate Loss */
-        amt = ms - me;
-        printf("Loss = %d", amt);
-    }
-    else
-    {
-        /* Neither gain nor loss */
-        printf(0);
-    }
