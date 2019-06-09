73a8f31ca43c4ff8c31bd140b96f00b4d145debf
Prodlax <50209414+Prodlax@users.noreply.github.com>
2019-06-08 19:34:50 -0700

    Update finalproj.php

--- finalproj.php
+++ finalproj.php
@@ -12,10 +12,68 @@
 
     <?php
        // define variables and set to empty values
-       $arg1 = $arg2 = $output = $retc = "";
+       $arg1 = $arg2 = $arg3 = $arg4 = $arg5 = $arg6 = $arg7 = $arg8 = $arg9 = $arg10 = $arg11 = $arg12 = $arg13 = $arg14 = $arg15 = $arg16 = $arg17 = $arg18 = $arg19 = $arg20 = $arg21 = $arg22 = $arg23 = $arg24 = $arg25 = $arg26 = $arg27 = $arg28 = $arg29 = $arg30 = $arg31 = $arg32 = $arg33 = $arg34 = $arg35 = $arg36 = $arg37 = $arg38 = $arg39 = $arg40 = $arg41 = $arg42 = $arg43 = $arg44 = $arg45 = $arg46 = $arg47 = $arg48 = $arg49 = $arg50 = $arg51 = $arg52 = $arg53 = $arg54 = $arg55 = $arg56 = $arg57 = $arg58 = $arg59 = $arg60 = $output = $retc = "";
        if ($_SERVER["REQUEST_METHOD"] == "POST") {
          $arg1 = test_input($_POST["arg1"]);
          $arg2 = test_input($_POST["arg2"]);
+         $arg3 = test_input($_POST["arg3"]);
+         $arg4 = test_input($_POST["arg4"]);
+         $arg5 = test_input($_POST["arg5"]);
+         $arg6 = test_input($_POST["arg6"]);
+         $arg7 = test_input($_POST["arg7"]);
+         $arg8 = test_input($_POST["arg8"]);
+         $arg9 = test_input($_POST["arg9"]);
+         $arg10 = test_input($_POST["arg1"]);
+         $arg1 = test_input($_POST["arg1"]);
+         $arg1 = test_input($_POST["arg1"]);
+         $arg1 = test_input($_POST["arg1"]);
+         $arg1 = test_input($_POST["arg1"]);
+         $arg1 = test_input($_POST["arg1"]);
+         $arg1 = test_input($_POST["arg1"]);
+         $arg1 = test_input($_POST["arg1]);
+         $arg1 = test_input($_POST["arg1"]);
+         $arg1 = test_input($_POST["arg1"]);
+         $arg2= test_input($_POST["arg2"]);
+         $arg2= test_input($_POST["arg1"]);
+         $arg2 = test_input($_POST["arg2"]);
+         $arg2= test_input($_POST["arg1"]);
+         $arg2 = test_input($_POST["arg2"]);
+         $arg2= test_input($_POST["arg1"]);
+         $arg2 = test_input($_POST["arg2"]);
+         $arg2 =test_input($_POST["arg1"]);
+         $arg2 = test_input($_POST["arg2"]);
+         $arg2= test_input($_POST["arg1"]);
+         $arg3= test_input($_POST["arg2"]);
+         $arg3 = test_input($_POST["arg1"]);
+         $arg3 = test_input($_POST["arg2"]);
+         $arg3 = test_input($_POST["arg1"]);
+         $arg3 = test_input($_POST["arg2"]);
+         $arg3 = test_input($_POST["arg1"]);
+         $arg3 = test_input($_POST["arg2"]);
+         $arg3 = test_input($_POST["arg1"]);
+         $arg3 = test_input($_POST["arg2"]);
+         $arg3 = test_input($_POST["arg1"]);
+         $arg4 = test_input($_POST["arg2"]);
+         $arg4 = test_input($_POST["arg1"]);
+         $arg4 = test_input($_POST["arg2"]);
+         $arg4 = test_input($_POST["arg1"]);
+         $arg4 = test_input($_POST["arg2"]);
+         $arg4 = test_input($_POST["arg1"]);
+         $arg4 = test_input($_POST["arg2"]);
+         $arg4 = test_input($_POST["arg1"]);
+         $arg4 = test_input($_POST["arg2"]);
+         $arg4 = test_input($_POST["arg1"]);
+         $arg5 = test_input($_POST["arg2"]);
+         $arg5 = test_input($_POST["arg1"]);
+         $arg5 = test_input($_POST["arg2"]);
+         $arg5 = test_input($_POST["arg1"]);
+         $arg5 = test_input($_POST["arg2"]);
+         $arg5 = test_input($_POST["arg1"]);
+         $arg5 = test_input($_POST["arg2"]);
+         $arg5 = test_input($_POST["arg1"]);
+         $arg5 = test_input($_POST["arg2"]);
+         $arg5 = test_input($_POST["arg1"]);
+         $arg6 = test_input($_POST["arg2"]);
          exec("/usr/lib/cgi-bin/pi/argtest2 " . $arg1 . " " . $arg2, $output, $retc); 
        }
        function test_input($data) {
